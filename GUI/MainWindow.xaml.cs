4f649f0f8359bf6eb2cf54ed6aacded589b18373
solarskit@student.agh.edu.pl <solarskit@student.agh.edu.pl>
2022-04-06 18:20:47 +0200

    podstawowe funkcjonalnosci w gui

---
+++ GUI/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ZespolyProj;
+
+namespace GUI
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        Zespol zespol;
+        public MainWindow()
+        {
+            InitializeComponent();
+            cmbNazwy.ItemsSource = Zespol.zwrocNazwyZespolow();
+        }
+
+        private void lstNazwy_DropDownClosed(object sender, EventArgs e)
+        {
+            zespol = Zespol.zwrocCalyZespol(cmbNazwy.Text);
+            if(zespol is object)
+            {
+                txtKierownik.Text = zespol.Kierownik.ToString();
+                dataGrid1.ItemsSource = Zespol.zwrocCzlonkowZespolu(cmbNazwy.Text);     
+            }
+        }
+    }
+}
